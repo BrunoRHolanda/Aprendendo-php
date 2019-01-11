a6cadf929bcb3dc7907b077aafe769aeb748da39
Bruno R. Holanda <bruno@engtecnologia.com>
2019-01-11 19:53:00 -0300

    mandando

--- Bookstone/src/Core/Helper/Router.php
+++ Bookstone/src/Core/Helper/Router.php
@@ -21,5 +21,40 @@ class Router {
 
 	public function route(Request $request): string {
 		$path = $request->getPath();
+
+		foreach ($this->routeMap as $route => $info) {
+			$regexRoute = $this->getRegexRoute($route, $info);
+			if (preg_match("@^/$regexRoute$@", $path)) {
+				return $this->executeController(
+					$route,$path, $info, $request
+				);
+			}
+		}
+
+		$errorController = new ErrorController($request);
+		return $errorController->notFound();
+	}
+
+	public function getRegexRoute(
+		string $route,
+		array $info
+	): string {
+		if (isset($info['params'])) {
+			foreach ($info['params'] as $name => $type) {
+				$route = str_replace(':'.$name,self::$regexPatters[$type], $route);
+			}
+		}
+
+		return $route;
+	}
+
+	public function extractParams(
+		string $route,
+		string $path
+	): array {
+		$params = [];
+
+		$pathParts = explode('/', $path);
+		$routeParts = explode('/', $route);
 	}
 }
